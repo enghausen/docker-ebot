235be58470e51dbb0c8339fbd0ec7d7bd709ebcc
enghausen <morten@enghausen.dk>
2020-11-03 14:21:05 +0100

    Update entrypoint.sh
    Change to avoid echo to $EBOT_HOME/config/plugins.ini on every container start. Was creating a entry on every startup. Also forgot to add the EXTERNAL_LOG_IP. Now using the domain name.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONTAINER_IP=$(hostname -i)
-EXTERNAL_IP="${EXTERNAL_IP:-}"
+DOMAIN="${DOMAIN:-}"
 MYSQL_HOST="${MYSQL_HOST:-mysql}"
 MYSQL_PORT="${MYSQL_PORT:-3306}"
 MYSQL_USER="${MYSQL_USER:-ebotv3}"
@@ -14,13 +14,13 @@ SSL_KEY_PATH="${SSL_KEY_PATH:-/ssl/key.pem}"
 
 LO3_METHOD="${LO3_METHOD:-restart}"
 KO3_METHOD="${KO3_METHOD:-restart}"
-DEMO_DOWNLOAD="${DEMO_DOWNLOAD:-true}"
-REMIND_RECORD="${REMIND_RECORD:-false}"
+DEMO_DOWNLOAD="${DEMO_DOWNLOAD:-false}"
+REMIND_RECORD="${REMIND_RECORD:-true}"
 DAMAGE_REPORT="${DAMAGE_REPORT:-false}"
 DELAY_READY="${DELAY_READY:-false}"
 USE_DELAY_END_RECORD="${USE_DELAY_END_RECORD:-true}"
 
-TOORNAMENT_PLUGIN_KEY="${TOORNAMENT_PLUGIN_KEY:-azertylol}"
+TOORNAMENT_PLUGIN_KEY="${TOORNAMENT_PLUGIN_KEY:-}"
 
 MAPS="${MAPS:-de_dust2_se,de_nuke_se,de_inferno_se,de_mirage_ce,de_train_se,de_cache,de_season,de_dust2,de_nuke,de_inferno,de_train,de_mirage,de_cbble,de_overpass}"
 # Split string by comma into an array
@@ -52,7 +52,7 @@ rm $CONFIG_FILE_TMP
 while ! nc -z $MYSQL_HOST $MYSQL_PORT; do sleep 3; done
 
 sed -i "s|BOT_IP =.*|BOT_IP = \"$CONTAINER_IP\"|" $CONFIG_FILE
-sed -i "s|EXTERNAL_LOG_IP = .*|EXTERNAL_LOG_IP = \"$EXTERNAL_IP\"|" $CONFIG_FILE
+sed -i "s|EXTERNAL_LOG_IP = .*|EXTERNAL_LOG_IP = \"$DOMAIN\"|" $CONFIG_FILE
 sed -i "s|MYSQL_IP =.*|MYSQL_IP = \"$MYSQL_HOST\"|" $CONFIG_FILE
 sed -i "s|MYSQL_PORT =.*|MYSQL_PORT = \"$MYSQL_PORT\"|" $CONFIG_FILE
 sed -i "s|MYSQL_USER =.*|MYSQL_USER = \"$MYSQL_USER\"|" $CONFIG_FILE
@@ -69,9 +69,7 @@ sed -i "s|DAMAGE_REPORT =.*|DAMAGE_REPORT = $DAMAGE_REPORT|" $CONFIG_FILE
 sed -i "s|DELAY_READY = .*|DELAY_READY = $DELAY_READY|" $CONFIG_FILE
 sed -i "s|USE_DELAY_END_RECORD = .*|USE_DELAY_END_RECORD = $USE_DELAY_END_RECORD|" $CONFIG_FILE
 
-echo -e "\n" >> $EBOT_HOME/config/plugins.ini
-echo '[\eBot\Plugins\Official\ToornamentNotifier]' >> $EBOT_HOME/config/plugins.ini
-echo "url=https://${EXTERNAL_IP}/matchs/toornament/export/{MATCH_ID}" >> $EBOT_HOME/config/plugins.ini
-echo "key=${TOORNAMENT_PLUGIN_KEY}" >> $EBOT_HOME/config/plugins.ini
+sed -i "s|url=.*|url=https://${DOMAIN}/matchs/toornament/export/{MATCH_ID}|" $EBOT_HOME/config/plugins.ini
+sed -i "s|key=.*|key=$TOORNAMENT_PLUGIN_KEY|" $EBOT_HOME/config/plugins.ini
 
 exec php "$EBOT_HOME/bootstrap.php" 
